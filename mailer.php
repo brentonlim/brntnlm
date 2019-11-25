5e85d2a8cdde49d2c3900bf22183b77005403c30
B. Lim <blim.digital@gmail.com>
2019-11-24 22:26:43 -0500

    editing portfolio.html, lightbox.js, lightbox.css

--- mailer.php
+++ mailer.php
@@ -62,7 +62,7 @@ if(isset($_POST['email'])) {
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>PORTFOLIO</title>
+    <title>MESSAGE SENT</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
